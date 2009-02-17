7bc8802007f3d017d85a346cae17ba2b7ec5bdcb
Darren Case <darren@darren-cases-macbook.local>
2009-02-17 15:10:37 -0500

    added separate blog menu partial

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,7 +3,7 @@ class MenusController < ApplicationController
      @site_sections = SiteSection.find(:all, :order => :position)
      
      respond_to do |format|
-       format.html { render :partial => 'common/main_menu' }# index.html.erb
+       format.html { render :partial => 'common/blog_menu' }# index.html.erb
        format.json { render :json => @site_sections }
        format.xml  { render :xml => @site_sections }
      end
