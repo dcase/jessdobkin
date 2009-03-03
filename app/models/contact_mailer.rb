2d3ab0c4c87606cd2c31dee6c8809359b1ed320d
Darren Case <darren@darren-cases-macbook.local>
2009-03-03 17:09:12 -0500

    changed recipient address on contact form

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
 
   def contact_form(contact)
     subject    'contact from jessdobkin.com'
-    recipients 'case_darren@yahoo.com'
+    recipients 'jess@jessdobkin.com'
     from       'jessdobkin.com <contact@jessdobkin.com>'
     sent_on    Time.now
     
